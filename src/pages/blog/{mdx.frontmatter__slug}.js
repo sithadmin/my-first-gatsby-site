673fb807254f191539ebc5784dba3ee66adb02f0
Jeff Miller <jeff@themillerfamily.xyz>
2022-09-08 09:02:49 -0400

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,12 +1,15 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
 const BlogPost = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt}></GatsbyImage>
       {children}
     </Layout>
   )
@@ -18,6 +21,12 @@ query ($id: String) {
     frontmatter {
       title
       date(formatString: "MMMM DD, YYYY")
+      hero_image_alt
+      hero_image {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
     }
   }
 }
